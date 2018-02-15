06c9cb3b337473a7a3ac19d519db3bb07c0666b0
billie <bpbilliepratama@gmail.com>
2018-02-16 00:10:56 +0700

    first commit

---
+++ build/billie/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/billie/catkin_ws/src/billie
+# Build directory: /home/billie/catkin_ws/build/billie
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
